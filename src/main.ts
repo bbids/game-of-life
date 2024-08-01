a4a207f30042eb2760cbf3a090769ce9e1b06545
bbids <82536500+bbids@users.noreply.github.com>
2024-08-01 20:56:51 +0200

    repair header css

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import './style.css';
 import './render';
 
 function setupAnomalyHeader() {
-  const anomalyHeader = document.querySelector("#anomaly-header") as HTMLElement;
+  const anomalyHeader = document.querySelector(".anomaly-header") as HTMLElement;
   const exitBtn = document.querySelector(".exit-btn") as HTMLElement;
 
   exitBtn.addEventListener('click', () => {
